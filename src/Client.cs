3669bc8d9436dcb75f7d5b6c736d4eb8aa95a30a
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-10-21 09:45:38 +0530

    Create Client.cs
    Add multifile/threaded downloads.

---
+++ src/Client.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+struct Job
+{
+    public string Path;
+    public string Hash;
+    public string Url;
+    public long Size;
+}
+
+static class Client
+{
+    static readonly HttpClient client = new();
+
+    static readonly SHA1 algorithm = SHA1.Create();
+
+    static XElement Parse(string address)
+    {
+        using var stream = client.GetStreamAsync(address).Result;
+        using var reader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+        return XElement.Load(reader);
+    }
+
+    internal static IList<Job> Get()
+    {
+        List<Job> jobs = [];
+
+        foreach (var element in Parse(Parse("https://piston-meta.mojang.com/v1/products/dungeons/f4c685912beb55eb2d5c9e0713fe1195164bba27/windows-x64.json")
+        .Descendants("url").First().Value).Element("files").Elements())
+        {
+            if (element.Element("type").Value[0] == 'd')
+            {
+                var path = element.Name(); if (!string.IsNullOrEmpty(path)) Directory.CreateDirectory(path);
+                continue;
+            }
+
+            var raw = element.Descendants("raw").First();
+            jobs.Add(new()
+            {
+                Path = element.Name(),
+                Hash = raw.Element("sha1").Value,
+                Size = long.Parse(raw.Element("size").Value),
+                Url = raw.Element("url").Value
+            });
+        }
+
+        jobs.Sort((x, y) => x.Size.CompareTo(y.Size)); return jobs;
+    }
+
+    internal static void Download(this IList<Job> source, Action<double, double, double> action)
+    {
+        double current = 0, total = source.Select(_ => _.Size).Sum();
+        Parallel.ForEach(source, (_) =>
+        {
+            using var stream = client.GetStreamAsync(_.Url).Result; using var destination = File.OpenWrite(_.Path);
+            var count = 0; var buffer = new byte[Environment.SystemPageSize];
+            while ((count = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                destination.Write(buffer, 0, count);
+                var progress = Math.Round((current += count) * 100 / total);
+                action(progress, current, total);
+            }
+        });
+    }
+
+    internal static IList<Job> Verify(this IList<Job> source, Action<int, int> action)
+    {
+        List<Job> jobs = [];
+
+        for (int index = 0; index < source.Count; index++)
+        {
+            var job = source[index];
+            if (File.Exists(job.Path))
+            {
+                using var stream = File.OpenRead(job.Path);
+                if (!job.Hash.Equals(BitConverter.ToString(algorithm.ComputeHash(stream)).Replace("-", string.Empty), StringComparison.OrdinalIgnoreCase)) jobs.Add(job);
+            }
+            action(index + 1, source.Count);
+        }
+
+        return jobs;
+    }
+
+    static string Name(this XElement source) => source.Name.NamespaceName.Length == 0 ? source.Name.LocalName : source.Attribute("item").Value;
+}
